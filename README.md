d8770185c032555b328061d769b4c600c6da60cc
Juan Carlos <jcge97@gmail.com>
2022-11-04 01:28:58 +0000

    Construcción del repositorio

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Git_Iris
+# **Ejemplo para trabajar con los comandos de git**.
+
+Mi intención con este pequeño proyecto es explicar cómo iniciarse en el mundo de GitHub mediante los comandos de **```Git```**. Para ello vamos a ver un ejemplo de cómo usaría estos comandos en mi entorno de trabajo para desde mi ordenador local y mediante el uso de la terminal/editor de texto, modificar localmente un repositorio almacenado en esta página. 
+
+He de decir que no soy un experto. Me acabo de iniciar, pero como aprender lo poco que se de ```git``` me pareció super poco intuitivo y begginer-friendly, pues lo comparto. Yo he aprendido lo básico. Y solo eso me ha facilitado la vida de manera sustancial a la hora de trabajar con esta página.
+
+## Comandos básicos (que es 80-90% de lo que se y uso):
+
+* ```git status```: te da información actual del estado de tu repositorio.
+
+* ```git clone``` : nos perimite clonar el repositorio de nuestra cuenta de GitHub a nuestro ordenados local.
+* ```git add``` : añade los cambios que se han realizado en algún directorio/archivo.
+* ```git commit -m``` : guarda los cambios y prepara al repositorio para ser "empujado" a tu cuenta online (debe hacerse después de ```git add```)
+* ```git pull``` y ```git push```: el primero extrae cambios hechos en la cuenta en la red, recomendado de usar siempre antes de hacer ningún cambio en el repositorio local de tu ordenador, especielmente si trabajas con otras personas en la página (esto lo he escuchado, no he llegado a tanto). ```git push``` por otro lado es lo contrario, empuja los resultados añadidos y guardados
---
+++ code/pca_iris.R
@@ -0,0 +1,93 @@
+# Primero cargamos las librerías que vamos a utilizar
+library(tidyverse)
+library(glue)
+library(ggthemes)
+library(openxlsx)
+library(ggtext)
+
+# Echamos un vistazo a los datos de iris esta base de 
+# datos viene de serie en R.
+# Las primeras 9 filas:
+iris %>% head(n=9)
+# El número de columnas:
+iris %>% ncol()
+# EL número de filas:
+iris %>% nrow()
+# Las especies de iris y su número:
+iris %>%
+    group_by(Species) %>%
+    count()
+# Guardamos los datos en datos crudos, para que parezca
+# un pipeline de verdad. xlsx no va en linux :(, usaremos
+# openxlsx: fail por el momento
+# write.table(iris, "data/raw/iris.csv", sep=",")
+# Como vamos a hacer un pca nos interesa únicamente
+# las variables cuantitativas por el momento
+matrix_iris <- iris[,-5] 
+# Vemos que nos hemos desecho de los grupos
+matrix_iris %>% colnames()
+
+# Vamos a usar prcomp:
+pca_iris <- prcomp(matrix_iris, scale=TRUE, center=TRUE)
+
+# Vemos el resumen del pca
+resumen_pca_iris <- summary(pca_iris);resumen_pca_iris
+
+# Vamos a calcular el % de la varianza de las primeras
+# componentes:
+var_pc1 <- round(resumen$importance[2,1]*100,2);var_pc1
+var_pc2 <- round(resumen$importance[2,2]*100,2);var_pc2
+# Varianza acumulada explicada de las dos primeras 
+# componentes:
+var_acumulada <- var_pc1 + var_pc2;var_acumulada
+
+# Vamos a crear un tibble de las dos primeras com_
+# ponentes, añadimos las especies también:
+
+pc_iris <- as_tibble(pca_iris$x[,c(1,2)]) %>%
+    mutate(species=iris$Species)
+# Vemos las primeras filas:
+pc_iris %>% head()
+
+# Graficamos los resultados
+ggiris <- pc_iris %>%
+    mutate(species=factor(species,
+                          levels=c("setosa","versicolor",
+                                   "virginica"),
+                          labels=c("Setosa", "Versicolor",
+                                   "Virginica"))) %>%
+    ggplot(aes(PC1,PC2,color=species, 
+               fill=species)) +
+    geom_point(pch=21,color="black") +
+    stat_ellipse(geom="polygon",alpha=.35,
+                 size=.75) +
+    labs(
+        title="PCA for the Species of iris",
+        subtitle="<span style='color:red'>?% of the acumulative varianze expalined</span>",
+        x="PC1 (?% varianze explained)",
+        y="PC2 (?% varianze explained)",
+        fill=NULL, color=NULL
+    ) +
+    scale_fill_manual(values=c("magenta","forestgreen",
+                                   "blue")) +
+    scale_color_manual(values=c("magenta","forestgreen",
+                                    "blue")) +
+    theme_classic() +
+    theme(
+        plot.title=element_text(hjust=.5,size=16,
+                                face="bold"),
+        plot.subtitle=element_markdown(hjust=.5, size=14,
+                                       face="bold"),
+        axis.title=element_text(size=12, face="bold"),
+        axis.text=element_text(size=11, face="bold"),
+        legend.background=element_rect(color="black",
+                                       fill=NULL),
+        legend.position=c(.4,.2),
+        legend.text=element_text(size=11)
+    ) 
+
+
+ggsave("images/pca_iris.png", 
+       width=6, height=5)
+
+getwd()
